f08e5424886b5f10472eadfc3ce4ddda023676a3
D3fau1t <sergey_97.09@mail.ru>
2016-07-11 11:17:38 +0600

    Operations
    Add simple operations

--- CALCULATOR/CALCULATOR/Form1.Designer.cs
+++ CALCULATOR/CALCULATOR/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CALCULATOR
 {
-    partial class Form1
+    partial class FormCalculator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,98 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.buttonMultiplication = new System.Windows.Forms.Button();
+            this.labelOutputSimbol = new System.Windows.Forms.Label();
+            this.textBoxInput1 = new System.Windows.Forms.TextBox();
+            this.textBoxInput2 = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonPlus
             // 
-            this.button1.Location = new System.Drawing.Point(12, 120);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonPlus.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(12, 207);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlus.TabIndex = 0;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
             // 
-            // button2
+            // buttonMinus
             // 
-            this.button2.Location = new System.Drawing.Point(93, 120);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonMinus.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMinus.Location = new System.Drawing.Point(93, 207);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(75, 23);
+            this.buttonMinus.TabIndex = 1;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
             // 
-            // button3
+            // buttonDivision
             // 
-            this.button3.Location = new System.Drawing.Point(174, 120);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonDivision.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDivision.Location = new System.Drawing.Point(174, 207);
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.Size = new System.Drawing.Size(75, 23);
+            this.buttonDivision.TabIndex = 2;
+            this.buttonDivision.Text = "/";
+            this.buttonDivision.UseVisualStyleBackColor = true;
+            this.buttonDivision.Click += new System.EventHandler(this.buttonDivision_Click);
             // 
-            // button4
+            // buttonMultiplication
             // 
-            this.button4.Location = new System.Drawing.Point(255, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonMultiplication.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMultiplication.Location = new System.Drawing.Point(255, 207);
+            this.buttonMultiplication.Name = "buttonMultiplication";
+            this.buttonMultiplication.Size = new System.Drawing.Size(75, 23);
+            this.buttonMultiplication.TabIndex = 3;
+            this.buttonMultiplication.Text = "*";
+            this.buttonMultiplication.UseVisualStyleBackColor = true;
+            this.buttonMultiplication.Click += new System.EventHandler(this.buttonMultiplication_Click);
             // 
-            // label1
+            // labelOutputSimbol
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(166, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.labelOutputSimbol.AutoSize = true;
+            this.labelOutputSimbol.Location = new System.Drawing.Point(166, 47);
+            this.labelOutputSimbol.Name = "labelOutputSimbol";
+            this.labelOutputSimbol.Size = new System.Drawing.Size(35, 13);
+            this.labelOutputSimbol.TabIndex = 4;
+            this.labelOutputSimbol.Text = "label1";
             // 
-            // textBox1
+            // textBoxInput1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.textBoxInput1.Location = new System.Drawing.Point(12, 12);
+            this.textBoxInput1.Name = "textBoxInput1";
+            this.textBoxInput1.Size = new System.Drawing.Size(327, 20);
+            this.textBoxInput1.TabIndex = 6;
             // 
-            // textBox2
+            // textBoxInput2
             // 
-            this.textBox2.Location = new System.Drawing.Point(224, 24);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 8;
+            this.textBoxInput2.Location = new System.Drawing.Point(12, 75);
+            this.textBoxInput2.Name = "textBoxInput2";
+            this.textBoxInput2.Size = new System.Drawing.Size(327, 20);
+            this.textBoxInput2.TabIndex = 8;
             // 
-            // label2
+            // labelOutput
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(166, 87);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(166, 136);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(35, 13);
+            this.labelOutput.TabIndex = 9;
+            this.labelOutput.Text = "label2";
             // 
-            // Form1
+            // FormCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 242);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.textBoxInput2);
+            this.Controls.Add(this.textBoxInput1);
+            this.Controls.Add(this.labelOutputSimbol);
+            this.Controls.Add(this.buttonMultiplication);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Name = "FormCalculator";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -128,14 +136,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Button buttonMultiplication;
+        private System.Windows.Forms.Label labelOutputSimbol;
+        private System.Windows.Forms.TextBox textBoxInput1;
+        private System.Windows.Forms.TextBox textBoxInput2;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
--- CALCULATOR/CALCULATOR/Form1.cs
+++ CALCULATOR/CALCULATOR/Form1.cs
@@ -10,11 +10,55 @@ using System.Windows.Forms;
 
 namespace CALCULATOR
 {
-    public partial class Form1 : Form
+    public partial class FormCalculator : Form
     {
-        public Form1()
+        public FormCalculator()
         {
             InitializeComponent();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            Convert.ToDouble(textBoxInput1.Text);
+            Convert.ToDouble(textBoxInput2.Text);
+            double First = Convert.ToDouble(textBoxInput1.Text);
+            double Second = Convert.ToDouble(textBoxInput2.Text);
+            double Result = First + Second;
+            labelOutput.Text = Result.ToString();
+            labelOutputSimbol.Text = buttonPlus.Text;
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            Convert.ToDouble(textBoxInput1.Text);
+            Convert.ToDouble(textBoxInput2.Text);
+            double First = Convert.ToDouble(textBoxInput1.Text);
+            double Second = Convert.ToDouble(textBoxInput2.Text);
+            double Result = First - Second;
+            labelOutput.Text = Result.ToString();
+            labelOutputSimbol.Text = buttonMinus.Text;
+        }
+
+        private void buttonDivision_Click(object sender, EventArgs e)
+        {
+            Convert.ToDouble(textBoxInput1.Text);
+            Convert.ToDouble(textBoxInput2.Text);
+            double First = Convert.ToDouble(textBoxInput1.Text);
+            double Second = Convert.ToDouble(textBoxInput2.Text);
+            double Result = First / Second;
+            labelOutput.Text = Result.ToString();
+            labelOutputSimbol.Text = buttonDivision.Text;
+        }
+
+        private void buttonMultiplication_Click(object sender, EventArgs e)
+        {
+            Convert.ToDouble(textBoxInput1.Text);
+            Convert.ToDouble(textBoxInput2.Text);
+            double First = Convert.ToDouble(textBoxInput1.Text);
+            double Second = Convert.ToDouble(textBoxInput2.Text);
+            double Result = First * Second;
+            labelOutput.Text = Result.ToString();
+            labelOutputSimbol.Text = buttonMultiplication.Text;
         }
     }
 }
